ca3c5c0d9e3d5322d16f4509fc8dbe4c6ea79f20
Matheus Muniz <103901768+MunizMat@users.noreply.github.com>
2023-02-19 12:21:31 -0300

    Alterações finais do projeto

--- src/models/HomeModel.js
+++ src/models/HomeModel.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
-// const HomeSchema = new mongoose.Schema({
+const HomeSchema = new mongoose.Schema({
+  titulo: { type: String, required: true },
+  descricao: String
+});
 
-// })
+const HomeModel = mongoose.model('Home', HomeSchema);
+
+class Home {
+
+}
+
+module.exports = Home;
